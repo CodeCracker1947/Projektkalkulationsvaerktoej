6cef22212cfb8a75b489ef124170e84345157e0b
Issa1011 <issatou1011@gmail.com>
2025-11-24 14:37:01 +0100

    mapper

---
+++ src/main/java/org/example/eksamenprojekt/EksamenProjektApplication.java
@@ -0,0 +1,13 @@
+package org.example.eksamenprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EksamenProjektApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EksamenProjektApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/eksamenprojekt/EksamenProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eksamenprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EksamenProjektApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
